9343ab37ec34b1e411ea961080b5b9b8eb25c058
bereleo <59530114+bereleo@users.noreply.github.com>
2021-12-04 22:22:13 -0300

    Update captura.py

--- captura.py
+++ captura.py
@@ -4,12 +4,11 @@ import os
 import time
 
 print(" LARGAMOS EL PROGRAMA PRINCIPAL ")
-print("Estamos agregando una rama de prueba")
-print("ESTOY PARA MEJORAR EL CODIGO EN RALIDAD VER DE EMPEZAR A EJECUTAR ALGO")
 
 #Haremos un ciclo para salvar mas de una imagen.
-#INGRESO DE NOMBRE DE ARCHIVO A SALVAR
 while True:
+  
+  #INGRESO DE NOMBRE DE ARCHIVO A SALVAR
   nombre=input("Ingrese el nombre del archivo a salvar sin extension [mipantalla]: ")
   # En caso de no dar nombre el defalul sera mipantalla
   if (nombre==""):
